3a085ecbe901d04d365ee65b530af30e40005218
Loresina <loresina86@gmail.com>
2024-03-13 21:38:37 +0700

    fix: CI/CD

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 
-import type { AppDispatch } from "../slices/slices";
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+// import type { AppDispatch } from "../slices/slices";
+// export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
